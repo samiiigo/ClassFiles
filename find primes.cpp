906dc8ed9504c7a685d7d1a379ad8c601997ce6f
Samyog Shrestha <184324726+samiiigo@users.noreply.github.com>
2025-01-15 13:03:40 -0600

    0

--- find primes.cpp
+++ find primes.cpp
@@ -13,7 +13,8 @@ int main() {
 
 bool checkPrime(int n) {
     if (n <= 1) return false;
-    for (int i = 2; i <= n / 2; i++) {
+    if (n%2 == 0) return false;
+    for (int i = 3; i <= sqrt(n); i+=2) {
         if (n % i == 0) return false;
     }
     return true;
