397779ff5b2d3eb4b7d5cca4ce64ad058be5d63a
Samyog Shrestha <184324726+samiiigo@users.noreply.github.com>
2025-01-17 22:26:29 -0600

    0

--- find primes.cpp
+++ find primes.cpp
@@ -8,11 +8,12 @@ int main() {
     int n;
     cin >> n;
 
-    cout << n << " is " << checkPrime(n) ? "prime" : "not prime" << endl;
+    cout << n << " is " << (checkPrime(n) ? "prime" : "not prime") << endl;
 }
 
 bool checkPrime(int n) {
     if (n <= 1) return false;
+    if (n == 2) return true;
     if (n%2 == 0) return false;
     for (int i = 3; i <= sqrt(n); i+=2) {
         if (n % i == 0) return false;
